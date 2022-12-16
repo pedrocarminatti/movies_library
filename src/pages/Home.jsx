5a2d2173fd2d7df3e8e7b6eae8a0f4af94b9eecc
Pedro <pedrolrc13@gmail.com>
2022-12-16 14:29:59 -0300

    Estilização do card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
+import "./MoviesGrid.css"
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
